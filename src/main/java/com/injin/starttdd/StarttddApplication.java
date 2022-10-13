c3984d4622155c0985f1fee028ceaacfed8b30a3
injinjeong <snowwwwww@naver.com>
2022-10-13 15:43:46 +0900

    first commit

---
+++ src/main/java/com/injin/starttdd/StarttddApplication.java
@@ -0,0 +1,13 @@
+package com.injin.starttdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarttddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarttddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/injin/starttdd/StarttddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.injin.starttdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarttddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
