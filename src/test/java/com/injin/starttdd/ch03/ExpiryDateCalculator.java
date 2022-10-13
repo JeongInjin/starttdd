658cbc20bb553a94d1d54c589d792b8ff60a486d
injinjeong <snowwwwww@naver.com>
2022-10-14 08:49:18 +0900

    ch03 작업중..

--- src/test/java/com/injin/starttdd/ch03/ExpiryDateCalculator.java
+++ src/test/java/com/injin/starttdd/ch03/ExpiryDateCalculator.java
@@ -3,7 +3,16 @@ package com.injin.starttdd.ch03;
 import java.time.LocalDate;
 
 public class ExpiryDateCalculator {
-    public LocalDate calculatorExpiryDate(LocalDate billingDate, int payAmount) {
-        return billingDate.plusMonths(1);
+
+    int addedMonths = 1;
+
+    public LocalDate calculatorExpiryDate(PayData payData) {
+        if (payData.getFirstBillingDate() != null) {
+            LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
+            if (payData.getFirstBillingDate().getDayOfMonth() != candidateExp.getDayOfMonth()) {
+                return candidateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth());
+            }
+        }
+        return payData.getBillingDate().plusMonths(1);
     }
 }
